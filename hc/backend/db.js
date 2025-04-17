ababd67c048c0b6ae440723ce03378f2a85c1f66
Manhaas10 <159009853+Manhaas10@users.noreply.github.com>
2025-04-17 20:48:09 +0530

    added .env and db.js

---
+++ hc/backend/db.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+const db = mysql.createConnection({
+  host: "localhost",
+  user:"root",
+  password: "password",
+  database: "databasename",
+});
+
+db.connect((err) => {
+    if (err) {
+      console.error("Database connection failed: " + err.stack);
+      return;
+    }
+    console.log("Connected to MySQL");
+  });
+
+module.exports = db;
