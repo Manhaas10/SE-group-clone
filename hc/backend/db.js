591bcad31d30bcdecc8aff34094fa9e367b70128
Manhaas10 <159009853+Manhaas10@users.noreply.github.com>
2025-05-02 20:25:43 +0530

    Update db.js

--- hc/backend/db.js
+++ hc/backend/db.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
 const mysql = require("mysql2");
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user:"root",
-  password: "",
-  database: "manhaasapp",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
-    if (err) {
-      console.error("Database connection failed: " + err.stack);
-      return;
-    }
-    console.log("Connected to MySQL");
-  });
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL");
+});
 
 module.exports = db;
